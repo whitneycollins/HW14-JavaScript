e0fb1c3ccda6c45167b53ed0581ce8793e2d01c5
Whitney Collins <w_collins0321@yahoo.com>
2019-08-22 06:41:53 -0700

    notes added to code

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,13 @@
+// get table references
 var tbody = d3.select("tbody");
 
+// loop through each row of data from data.js
 data.forEach((sightings) => {
+    //  append a row and cells for each value in the row to the table body
     var row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
     Object.entries(sightings).forEach(([key,value]) =>{
         var cell = row.append("td");
         cell.text(value);
@@ -14,13 +20,16 @@ button.on("click", function() {
     
     var filteredData = data;
 
-    // Select the input element and get the raw HTML node
+  // Grab the datetime value from the filter
     var inputElement = d3.select("#datetime");
 
     var inputValue = inputElement.property("value").trim();
   
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
     filteredData = filteredData.filter(data => data.datetime === inputValue);
 
+    // clear out any existing data
     tbody.html("")
 
     //displayTable(tableData);
